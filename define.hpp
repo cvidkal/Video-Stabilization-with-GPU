8496223733d2edd986cf52d3adb6604dbb007295
fg-king <cgjdemo@gmail.com>
2014-07-31 16:41:14 +0800

    First Commit

---
+++ define.hpp
@@ -0,0 +1,15 @@
+#ifndef __DEFINE_HPP__
+#define __DEFINE_HPP__
+
+#define cvidkal
+//#define FileSource
+#define reflash1
+//#define reflash2
+extern bool reflash_flag;
+//#define MYHOMOGRAPHY
+#define DEBUG
+#define Intel
+//#define TimeDebug
+//#define  HD720p
+#define Low320p
+#endif // __DEFINE_HPP__
